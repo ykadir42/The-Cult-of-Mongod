6145b2891e82ae160f497ccc5f5800b9c9ec1602
ibnullify <ijahan1@stuy.edu>
2018-02-15 12:14:49 -0500

    api

--- mongo.py
+++ mongo.py
@@ -4,7 +4,7 @@
 # 2018-02-14  
 
 from pymongo import MongoClient
-
+import json, urllib2
 #connects to server
 c = MongoClient('lisa.stuy.edu', 27017)
 #switches to test database
@@ -54,45 +54,10 @@ find_by_zip_code_and_score_threshold("10282", 10)
 find_by_building_and_cusine("102", "American")
 
 
-'''Ibnul Jahan
-SoftDev1 pd7
-HW13 -- A RESTful Journey Skyward
-2017-11-08
-
-
-
-import urllib2
-from flask import Flask, render_template, request, session, redirect, url_for
-import json
-
-
-"""api_key = "1I3RG0HH1aH67s0fi9nWhhc5oudr3UvzxprRK35f"
-data_file = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key="+ api_key)
-info = data_file.read()
-data = json.loads(info)
-img= data['hdurl']
-explanation= data['explanation']
-"""
-
-
-
-app = Flask(__name__) #create instance of class 
-
-#assign following fxn to run when
-#root route requested
-
-@app.route("/")
-def hello():
-    data_file = urllib2.urlopen("http://jservice.io/api/random")
+def api():
+    data_file = urllib2.urlopen("http://api.giphy.com/v1/gifs/search?q=make+it+rain&api_key=dc6zaTOxFJmzC")
     info = data_file.read()
-    data = json.loads(info)
-    question= data[0]['question']
-    answer= data[0]['answer']
-    category = data[0]['category']['title']
-    return render_template("temp.html", title = "Jeopardy", category = category, question = question, answer = answer)
+    datas = json.loads(info)
+    print datas['data']
 
-
-if __name__=="__main__":
-    app.debug = True
-app.run()
-'''
+api()
